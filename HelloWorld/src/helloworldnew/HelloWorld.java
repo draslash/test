f0e744482e1f7122ccd80e0fb1399e54991cb613
Aaron <aaron_invictus@hotmail.com>
2016-03-06 23:16:22 +0800

    changing into new folder

---
+++ HelloWorld/src/helloworldnew/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworldnew;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DIAM LA");
+		System.out.println("can this work");
+	}
+
+}
